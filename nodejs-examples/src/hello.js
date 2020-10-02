e2ce1506554148b7b9d739fcde23de00017f5e53
Zemian Deng <zemian.deng@oracle.com>
2020-10-01 23:59:29 -0400

    Cleanup hello

--- nodejs-examples/src/hello.js
+++ nodejs-examples/src/hello.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg')
-const client = new Client()
-;(async () => {
+const client = new Client();
+
+(async () => {
   await client.connect()
   const res = await client.query('SELECT $1::text as message', ['Hello world!'])
   console.log(res.rows[0].message) // Hello world!
